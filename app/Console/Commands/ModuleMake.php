5965751d105aab422fc931619dc9849480fa2db3
Roman <kryvoboker@gmail.com>
2022-01-16 21:48:10 +0200

    created functions for modules

--- app/Console/Commands/ModuleMake.php
+++ app/Console/Commands/ModuleMake.php
@@ -3,10 +3,13 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Contracts\Filesystem\FileNotFoundException;
+use Illuminate\Filesystem\Filesystem;
 use Illuminate\Support\Str;
 
 class ModuleMake extends Command
 {
+    private $files;
     /**
      * The name and signature of the console command.
      *
@@ -32,15 +35,18 @@ class ModuleMake extends Command
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(Filesystem $filesystem)
     {
         parent::__construct();
+
+        $this->files = $filesystem;
     }
 
     /**
      * Execute the console command.
      *
-     * @return int
+     * @return void
+     * @throws FileNotFoundException
      */
     public function handle()
     {
@@ -87,26 +93,107 @@ class ModuleMake extends Command
 
     private function createModel()
     {
-
         $model = Str::singular(Str::studly(class_basename($this->argument('name'))));
         $this->call('make:model', [
             'name' => "App\\Modules\\" . trim($this->argument('name')) . "\\Models\\" . $model
         ]);
     }
 
+    /**
+     * @throws FileNotFoundException
+     */
     private function createController()
     {
+        $controller = Str::studly(class_basename($this->argument('name')));
+        $modelName = Str::singular(Str::studly(class_basename($this->argument('name'))));
 
+        $path = $this->getControllerPath($this->argument('name'));
+
+        if ($this->alreadyExists($path)) {
+
+            $this->error('Controller already exists!!!');
+        } else {
+
+            $this->makeDirectory($path);
+
+            $stub = $this->files->get(base_path('resources/stubs/controller.model.api.stub'));
+
+            $stub = str_replace(
+                [
+                    'DummyNamespace',
+                    'DummyRootNamespace',
+                    'DummyClass',
+                    'DummyFullModelClass',
+                    'DummyModelClass',
+                    'DummyModelVariable'
+                ],
+                [
+                    'App\\Modules\\' . trim($this->argument('name')) . '\\Controllers',
+                    $this->laravel->getNamespace(),
+                    $controller . 'Controller',
+                    'App\\Modules\\' . trim($this->argument('name')) . "\\Models\\{$modelName}",
+                    $modelName,
+                    lcfirst(($modelName))
+                ],
+                $stub
+            );
+
+            $this->files->put($path, $stub);
+            $this->info('Controller created successfully');
+//            $this->updateModularConfig();
+        }
+        $this->createRoutes($controller, $modelName);
     }
 
+    /**
+     * @throws FileNotFoundException
+     */
     private function createApiController()
     {
+        $controller = Str::studly(class_basename($this->argument('name')));
+        $modelName = Str::singular(Str::studly(class_basename($this->argument('name'))));
 
+        $path = $this->getApiControllerPath($this->argument('name'));
+
+        if ($this->alreadyExists($path)) {
+
+            $this->error('API controller already exists!!!');
+        } else {
+
+            $this->makeDirectory($path);
+
+            $stub = $this->files->get(base_path('resources/stubs/controller.model.api.stub'));
+
+            $stub = str_replace(
+                [
+                    'DummyNamespace',
+                    'DummyRootNamespace',
+                    'DummyClass',
+                    'DummyFullModelClass',
+                    'DummyModelClass',
+                    'DummyModelVariable'
+                ],
+                [
+                    'App\\Modules\\' . trim($this->argument('name')) . '\\Controllers\\Api',
+                    $this->laravel->getNamespace(),
+                    $controller . 'Controller',
+                    'App\\Modules\\' . trim($this->argument('name')) . "\\Models\\{$modelName}",
+                    $modelName,
+                    lcfirst(($modelName))
+                ],
+                $stub
+            );
+
+            $this->files->put($path, $stub);
+            $this->info('API controller created successfully');
+//            $this->updateModularConfig();
+        }
+
+        $this->createApiRoutes($controller, $modelName);
     }
 
     private function createMigration()
     {
-
         try {
 
             $table = Str::plural(Str::snake(class_basename($this->argument('name'))));
@@ -121,13 +208,195 @@ class ModuleMake extends Command
         }
     }
 
+    /**
+     * @throws FileNotFoundException
+     */
     private function createVueComponent()
     {
+        $path = $this->getVueComponentPath($this->argument('name'));
 
+        $component = Str::studly(class_basename($this->argument('name')));
+
+        if ($this->alreadyExists($path)) {
+            $this->error('Vue Component already exists!');
+        } else {
+            $this->makeDirectory($path);
+
+            $stub = $this->files->get(base_path('resources/stubs/vue.component.stub'));
+
+            $stub = str_replace(
+                [
+                    'DummyClass',
+                ],
+                [
+                    $component,
+                ],
+                $stub
+            );
+
+            $this->files->put($path, $stub);
+            $this->info('Vue Component created successfully.');
+        }
     }
 
+    /**
+     * @throws FileNotFoundException
+     */
     private function createView()
     {
+        $paths = $this->getViewPath($this->argument('name'));
 
+        foreach ($paths as $path) {
+            $view = Str::studly(class_basename($this->argument('name')));
+
+            if ($this->alreadyExists($path)) {
+                $this->error('View already exists!');
+            } else {
+                $this->makeDirectory($path);
+
+                $stub = $this->files->get(base_path('resources/stubs/view.stub'));
+
+                $stub = str_replace(
+                    [
+                        '',
+                    ],
+                    [
+                    ],
+                    $stub
+                );
+
+                $this->files->put($path, $stub);
+                $this->info('View created successfully.');
+            }
+        }
+    }
+
+    private function getControllerPath($argument) : string
+    {
+        $controller = Str::studly(class_basename($argument));
+        return $this->laravel['path'] . '/Modules/' . str_replace('\\', '/', $argument) . '/Controllers/' . "{$controller}Controller.php";
+    }
+
+    private function getApiControllerPath($name) : string
+    {
+        $controller = Str::studly(class_basename($name));
+        return $this->laravel['path'] . '/Modules/' . str_replace('\\', '/', $name) . '/Controllers/Api/' . "{$controller}Controller.php";
+    }
+
+    private function makeDirectory($path)
+    {
+        if (!$this->files->isDirectory(dirname($path))) {
+
+            $this->files->makeDirectory(dirname($path), 0777, true, true);
+        }
+
+        return $path;
+    }
+
+    /**
+     * @throws FileNotFoundException
+     */
+    private function createRoutes(string $controller, string $modelName) : void
+    {
+        $routePath = $this->getRoutesPath($this->argument('name'));
+
+        if ($this->alreadyExists($routePath)) {
+
+            $this->error('Routes already exists!!');
+        } else {
+
+            $this->makeDirectory($routePath);
+
+            $stub = $this->files->get(base_path('resources/stubs/routes.web.stub'));
+
+            $stub = str_replace(
+                [
+                    'DummyClass',
+                    'DummyRoutePrefix',
+                    'DummyModelVariable'
+                ],
+                [
+                    $controller . 'Controller',
+                    Str::plural(Str::snake(lcfirst($modelName), '-')),
+                    lcfirst(($modelName))
+                ],
+                $stub
+            );
+
+            $this->files->put($routePath, $stub);
+            $this->info('Routes created successfully');
+        }
+    }
+
+    /**
+     * @throws FileNotFoundException
+     */
+    private function createApiRoutes(string $controller, string $modelName) : void
+    {
+        $routePath = $this->getApiRoutesPath($this->argument('name'));
+
+        if ($this->alreadyExists($routePath)) {
+
+            $this->error('API routes already exists!!');
+        } else {
+
+            $this->makeDirectory($routePath);
+
+            $stub = $this->files->get(base_path('resources/stubs/routes.api.stub'));
+
+            $stub = str_replace(
+                [
+                    'DummyClass',
+                    'DummyRoutePrefix',
+                    'DummyModelVariable'
+                ],
+                [
+                    $controller . 'Controller',
+                    Str::plural(Str::snake(lcfirst($modelName), '-')),
+                    lcfirst(($modelName))
+                ],
+                $stub
+            );
+
+            $this->files->put($routePath, $stub);
+            $this->info('API routes created successfully');
+        }
+    }
+
+    private function getRoutesPath($name) : string
+    {
+        return $this->laravel['path'] . '/Modules/' . str_replace('\\', '/', $name) . "/Routes/web.php";
+    }
+
+    private function getApiRoutesPath($name) : string
+    {
+        return $this->laravel['path'] . '/Modules/' . str_replace('\\', '/', $name) . "/Routes/api.php";
+    }
+
+    protected function alreadyExists(string $path) : bool
+    {
+        return $this->files->exists($path);
+    }
+
+    protected function getVueComponentPath($name) : string
+    {
+        return base_path('resources/js/components/'.str_replace('\\', '/', $name).".vue");
+    }
+
+    protected function getViewPath($name) : object
+    {
+        $arrFiles = collect([
+            'create',
+            'edit',
+            'index',
+            'show',
+        ]);
+
+        //str_replace('\\', '/', $name)
+        $paths = $arrFiles->map(function($item) use ($name){
+            return base_path('resources/views/'.str_replace('\\', '/', $name).'/'.$item.".blade.php");
+        });
+
+        return $paths;
     }
 }
